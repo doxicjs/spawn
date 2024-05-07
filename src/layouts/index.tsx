f56884c355039c5b04e8130ea2df9a84fb14b3b7
thejskhan <thejargalsaikhan@gmail.com>
2024-05-07 22:21:58 +0800

    removed unnecessary code

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -4,9 +4,7 @@ export default function Layout() {
   return (
     <>
       <header className="bg-light border-line fixed top-0 z-10 w-full border-b">
-        <nav className="mx-auto flex h-16 w-full max-w-screen-xl items-center">
-          <span className="font-semibold">Doxic Spawn</span>
-        </nav>
+        <nav className="mx-auto flex h-14 w-full max-w-screen-xl items-center"></nav>
       </header>
       <main className="mx-auto w-full max-w-screen-xl">
         <Outlet />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,7 @@
-import { useExampleStore } from '@/shared/stores/example'
-import FoobarForm from '@/components/Form/Foobar'
-import { Button } from '@/components/ui/button'
-
 export default function Home() {
-  const { count, increment, decrement } = useExampleStore()
-
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
       <h1 className="text-4xl font-medium">Doxic Spawn</h1>
-      <span className="text-4xl font-medium">{count}</span>
-      <div className="flex w-full max-w-max gap-2">
-        <Button onClick={increment}>Increment</Button>
-        <Button onClick={decrement}>Decrement</Button>
-      </div>
-
-      <FoobarForm />
     </div>
   )
 }
