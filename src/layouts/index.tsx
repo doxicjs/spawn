d6dbfd1fa0724ae1d5da43daa4a9fb4deeb85e4f
thejskhan <thejargalsaikhan@gmail.com>
2024-09-11 14:55:36 +0800

    fix unused colors

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 export default function Layout() {
   return (
     <>
-      <header className="bg-light border-line fixed top-0 z-10 w-full border-b">
+      <header className="fixed top-0 z-10 w-full border-b bg-background">
         <nav className="mx-auto flex h-14 w-full max-w-screen-xl items-center"></nav>
       </header>
       <main className="mx-auto w-full max-w-screen-xl">
