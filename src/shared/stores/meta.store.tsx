b7daa9557fa2fd3640624f08c901f899232bde1e
2khan <2khanvas@gmail.com>
2025-06-29 03:06:26 +0800

    sync

--- src/shared/stores/meta.store.tsx
+++ src/shared/stores/meta.store.tsx
@@ -1,35 +1,52 @@
+import { useEffect } from 'react'
+
+import { useLocation } from 'react-router-dom'
+import { Fragment } from 'react/jsx-runtime'
 import { create } from 'zustand'
-import { immer } from 'zustand/middleware/immer'
 
-type ExampleStore = {
-  count: number
+import { get_route_meta } from '@/shared/utils/router'
+import type { TMeta } from '@/shared/utils/router'
+
+type MetaStore = {
+  meta: TMeta
+  handlers: {
+    set_meta: (meta: TMeta) => void
+    reset: () => void
+  }
 }
 
-type Actions = {
-  increment: () => void
-  decrement: () => void
-  reset: () => void
+const IMeta: TMeta = {
+  title: import.meta.env.VITE_APP_TITLE
 }
 
-const IState: ExampleStore = {
-  count: 0
-}
+export const useMeta = create<MetaStore>()((set) => ({
+  meta: IMeta,
+  handlers: {
+    set_meta: (meta) => set({ meta }),
+    reset: () => set({ meta: IMeta })
+  }
+}))
 
-const ResetState: ExampleStore = {
-  count: 0
-}
+export function Meta() {
+  const { pathname } = useLocation()
+  const meta = useMeta((s) => s.meta)
+  const { set_meta, reset } = useMeta((s) => s.handlers)
 
-export const useExampleStore = create(
-  immer<ExampleStore & Actions>((set) => ({
-    ...IState,
-    increment: () =>
-      set((state) => {
-        state.count += 1
-      }),
-    decrement: () =>
-      set((state) => {
-        state.count -= 1
-      }),
-    reset: () => set(ResetState)
-  }))
-)
+  useEffect(() => {
+    const meta = get_route_meta(pathname)
+
+    if (!meta) {
+      reset()
+      return
+    }
+    set_meta(meta)
+  }, [pathname, set_meta, reset])
+
+  return (
+    <Fragment>
+      <title>{meta.title}</title>
+      <meta name="title" content={meta.title} />
+      <meta name="description" content={meta.description} />
+    </Fragment>
+  )
+}
